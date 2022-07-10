936e1229da891af25b93ffd5c9b75bda1e7fccf6
nopbreak <nopbreak@gmail.com>
2022-07-10 10:20:26 +0300

    core: try fix 0 user error

---
+++ app/app/src/main/java/tv/twitch/android/shared/leaderboards/repository/pubsub/LeaderboardPubSubUpdater.java
@@ -0,0 +1,18 @@
+package tv.twitch.android.shared.leaderboards.repository.pubsub;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import tv.twitch.android.models.leaderboard.ChannelLeaderboardRank;
+import tv.twitch.android.shared.leaderboards.model.LeaderboardPubSubRankingUpdate;
+
+public class LeaderboardPubSubUpdater {
+    private final Single<LeaderboardPubSubRankingUpdate> normalizePubSubRanking(LeaderboardPubSubUpdate leaderboardPubSubUpdate, List<ChannelLeaderboardRank> list) {
+        LeaderboardPubSubEntry entry = null;
+        if (!entry.getUserId().equals("") && entry.getUserId().equals("0")) { // TODO: __INJECT_CODE
+            //
+        }
+
+        return null;
+    }
+}

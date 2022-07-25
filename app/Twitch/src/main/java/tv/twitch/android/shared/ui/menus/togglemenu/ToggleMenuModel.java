a12b279f6b40375bea06eeed933f2a8a392fede8
nopcan <r3kPPK##.k>
2022-07-25 09:31:57 +0300

    snapshot

--- app/Twitch/src/main/java/tv/twitch/android/shared/ui/menus/togglemenu/ToggleMenuModel.java
+++ app/Twitch/src/main/java/tv/twitch/android/shared/ui/menus/togglemenu/ToggleMenuModel.java
@@ -4,15 +4,15 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
-import tv.twitch.android.core.adapters.RecyclerAdapterItem;
 import tv.twitch.android.core.mvp.lifecycle.VisibilityProvider;
 import tv.twitch.android.shared.ui.menus.SettingActionListener;
 import tv.twitch.android.shared.ui.menus.SettingsPreferencesController;
-import tv.twitch.android.shared.ui.menus.formvalue.FormValueDelegate;
+import tv.twitch.android.shared.ui.menus.core.MenuModel;
 
-public class ToggleMenuModel extends FormValueDelegate<Boolean> { // TODO: __REMOVE_FINAL
-    public ToggleMenuModel(String var1, String var2, String var3, boolean var4, boolean var5, Drawable var6, String var7, boolean var8, String var9, Integer var10, Integer var11, SettingsPreferencesController.SettingsPreference var12, View.OnClickListener var13) {
-        super(var1, var2, var3, var6, var4, var5);
+
+public class ToggleMenuModel extends MenuModel.SingleItemModel { // TODO: __REMOVE_FINAL
+    public ToggleMenuModel(String primaryText, String secondaryText, String auxiliaryText, boolean toggleState, boolean isEnabled, Drawable drawable, String eventName, boolean includeBackground, String pillText, Integer pillColor, Integer pillTextColor, SettingsPreferencesController.SettingsPreference settingsPref, View.OnClickListener titleClickListener) {
+        super(null, null, null, null, null);
     }
 
     public final String getEventName() {
@@ -20,7 +20,7 @@ public class ToggleMenuModel extends FormValueDelegate<Boolean> { // TODO: __REM
     }
 
     @Override
-    protected RecyclerAdapterItem toRecyclerAdapterItem(Context context, SettingActionListener settingActionListener, VisibilityProvider visibilityProvider) {
+    public ToggleMenuRecyclerItem toRecyclerAdapterItem(Context context, SettingActionListener settingActionListener, VisibilityProvider visibilityProvider) {
         return null;
     }
 }

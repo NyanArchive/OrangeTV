b2b0a175ce7c162cb55bdb59a5d8575d7550634e
nopbreak <nopbreak@gmail.com>
2022-08-07 10:39:28 +0300

    Simplify SettingsController

--- app/features/settings/src/main/java/tv/orange/features/settings/component/SettingsController.kt
+++ app/features/settings/src/main/java/tv/orange/features/settings/component/SettingsController.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import tv.orange.core.Logger
 import tv.orange.core.PreferenceManager
 import tv.orange.core.models.flag.Flag
-import tv.orange.core.models.flag.variants.DeletedMessages
+import tv.orange.core.models.flag.Internal
 import tv.orange.core.models.flag.variants.PlayerImpl
 import tv.orange.features.settings.bridge.model.DropDownMenuModelExt
 import tv.orange.features.settings.bridge.model.FlagToggleMenuModelExt
@@ -25,24 +25,12 @@ class SettingsController @Inject constructor() : SettingsPreferencesController {
     }
 
     fun getMainSettingModels(context: Context): Collection<MenuModel> {
-        return listOf(
-            FlagToggleMenuModelExt(Flag.DEV_MODE),
-            FlagToggleMenuModelExt(Flag.BTTV_EMOTES),
-            FlagToggleMenuModelExt(Flag.FFZ_EMOTES),
-            FlagToggleMenuModelExt(Flag.STV_EMOTES),
-            FlagToggleMenuModelExt(Flag.FFZ_BADGES),
-            FlagToggleMenuModelExt(Flag.STV_BADGES),
-            FlagToggleMenuModelExt(Flag.CHA_BADGES),
-            FlagToggleMenuModelExt(Flag.CHE_BADGES),
-            FlagToggleMenuModelExt(Flag.STV_AVATARS),
-            FlagToggleMenuModelExt(Flag.CHAT_HISTORY),
-            FlagToggleMenuModelExt(Flag.CHAT_TIMESTAMPS),
-            FlagToggleMenuModelExt(Flag.DISABLE_STICKY_HEADERS_EP),
-            FlagToggleMenuModelExt(Flag.HIDE_BITS_BUTTON),
-            FlagToggleMenuModelExt(Flag.DISABLE_LINK_DISCLAIMER),
-            FlagToggleMenuModelExt(Flag.HIDE_LEADERBOARDS),
-            DropDownMenuModelExt<PlayerImpl>(Flag.PLAYER_IMPL, context),
-            DropDownMenuModelExt<DeletedMessages>(Flag.DELETED_MESSAGES, context),
-        )
+        return Flag.values().mapNotNull {
+            when (it.value) {
+                is Internal.BooleanValue -> FlagToggleMenuModelExt(it)
+                is Internal.ListValue<*> -> DropDownMenuModelExt<PlayerImpl>(it, context)
+                else -> null
+            }
+        }
     }
 }

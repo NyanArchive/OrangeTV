0c1431aa0187d6102b1ed554c9bd813ae1537c59
nopbreak <nopbreak@gmail.com>
2022-08-12 09:24:12 +0300

    Fix typo

--- app/models/src/main/java/tv/orange/models/abc/EmotePackageSet.kt
+++ app/models/src/main/java/tv/orange/models/abc/EmotePackageSet.kt
@@ -3,8 +3,8 @@ package tv.orange.models.abc
 enum class EmotePackageSet(val value: String, val stringId: String) {
     BttvChannel("BTTV-CHANNEL", "orange_emotecard_bttv_channel"),
     FfzChannel("FFZ-CHANNEL", "orange_emotecard_ffz_channel"),
-    StvChannel("STV-CHANNEL", "orange_emotecard_stv_channel"),
+    StvChannel("7TV-CHANNEL", "orange_emotecard_stv_channel"),
     BttvGlobal("BTTV-GLOBAL", "orange_emotecard_bttv_global"),
     FfzGlobal("FFZ-GLOBAL", "orange_emotecard_ffz_global"),
-    StvGlobal("STV-GLOBAL", "orange_emotecard_stv_global")
+    StvGlobal("7TV-GLOBAL", "orange_emotecard_stv_global")
 }

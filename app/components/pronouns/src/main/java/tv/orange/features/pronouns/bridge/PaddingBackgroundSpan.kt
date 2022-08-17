3d969389b79bbbe1113b55f4a86276177f02e84d
nopbreak <nopbreak@gmail.com>
2022-08-17 11:39:35 +0300

    Simplify PaddingBackgroundSpan.kt

--- app/components/pronouns/src/main/java/tv/orange/features/pronouns/bridge/PaddingBackgroundSpan.kt
+++ app/components/pronouns/src/main/java/tv/orange/features/pronouns/bridge/PaddingBackgroundSpan.kt
@@ -3,20 +3,17 @@ package tv.orange.features.pronouns.bridge
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
-import android.text.style.LineHeightSpan
 import android.text.style.ReplacementSpan
 
 /* ...
     Source: https://gist.github.com/anacicconi/39469b975db3edb81adce3a7d199133b
  */
-class RoundedBackgroundSpan(
+class PaddingBackgroundSpan(
     private val backgroundColor: Int,
     private val textColor: Int,
-    private val padding: Int = 16,
-    private val radius: Float? = null
-) : ReplacementSpan(),
+    private val padding: Int = 16
+) : ReplacementSpan() {
 
-    LineHeightSpan {
     override fun getSize(
         paint: Paint,
         text: CharSequence,
@@ -41,31 +38,11 @@ class RoundedBackgroundSpan(
         paint.color = backgroundColor
 
         val width = paint.measureText(text, start, end)
-        val height = paint.fontMetrics.descent - paint.fontMetrics.ascent + 2 * padding
 
-        if (radius != null) {
-            val rect = RectF(x, top.toFloat(), x + width + 2 * padding, bottom.toFloat())
-            canvas.drawRoundRect(rect, radius, radius, paint)
-        } else {
-            canvas.drawCircle(
-                x + ((width + 2 * padding) / 2),
-                ((bottom - top) / 2).toFloat(),
-                height / 2,
-                paint
-            )
-        }
+        val rect = RectF(x, top.toFloat(), x + width + 2 * padding, bottom.toFloat())
+        canvas.drawRect(rect, paint)
 
         paint.color = textColor
         canvas.drawText(text, start, end, x + padding, y.toFloat(), paint)
-    }
-
-    override fun chooseHeight(
-        text: CharSequence,
-        start: Int,
-        end: Int,
-        spanstartv: Int,
-        lineHeight: Int,
-        fm: Paint.FontMetricsInt?
-    ) {
     }
 }

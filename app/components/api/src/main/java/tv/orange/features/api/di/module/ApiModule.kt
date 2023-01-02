c993ec7661cf30e397b527c80a1e13ecc14d6e6e
nopbreak <nopbreak@gmail.com>
2023-01-02 18:42:34 +0300

    refactoring

--- app/components/api/src/main/java/tv/orange/features/api/di/module/ApiModule.kt
+++ app/components/api/src/main/java/tv/orange/features/api/di/module/ApiModule.kt
@@ -4,166 +4,166 @@ import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import tv.orange.features.api.component.data.api.*
-import tv.orange.features.api.di.scope.ApiScope
+import tv.orange.features.api.di.scope.*
 import javax.inject.Named
 
 @Module
 class ApiModule {
     @ApiScope
     @Provides
-    @Named("bttv")
+    @Named(BTTV)
     fun provideBttvRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.betterttv.net/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("stv")
+    @Named(STV)
     fun provideStvRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.7tv.app/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("ffz")
+    @Named(FFZ)
     fun provideFfzRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.frankerfacez.com/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("chatterino")
+    @Named(CHATTERINO)
     fun provideChatterinoRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.chatterino.com/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("itz")
+    @Named(ITZ)
     fun provideItzRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://itzalex.github.io/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("nop")
+    @Named(NOP)
     fun provideNopRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.nopbreak.ru/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("ttsft")
+    @Named(TTSFT)
     fun provideTTSFTRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.twitch.tyo.kwabang.net/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("twitching")
+    @Named(TWITCHING)
     fun provideTwitchingRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://workers.twitch-relay.wesub.io/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("lol")
+    @Named(TTVLOL)
     fun provideLolRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.ttv.lol/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("pas1")
+    @Named(PAS1)
     fun providePas1RetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://eu1.jupter.ga/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("pas2")
+    @Named(PAS2)
     fun providePas2RetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://eu2.jupter.ga/").build()
     }
 
     @ApiScope
     @Provides
-    @Named("ffzap")
+    @Named(FFZAP)
     fun provideFfzapRetrofitClient(builder: Retrofit.Builder): Retrofit {
         return builder.baseUrl("https://api.ffzap.com/").build()
     }
 
     @ApiScope
     @Provides
-    fun provideBttvApi(@Named("bttv") retrofit: Retrofit): BttvApi {
+    fun provideBttvApi(@Named(BTTV) retrofit: Retrofit): BttvApi {
         return retrofit.create(BttvApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideItzApi(@Named("itz") retrofit: Retrofit): ItzalexApi {
+    fun provideItzApi(@Named(ITZ) retrofit: Retrofit): ItzalexApi {
         return retrofit.create(ItzalexApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideStvApi(@Named("stv") retrofit: Retrofit): StvApi {
+    fun provideStvApi(@Named(STV) retrofit: Retrofit): StvApi {
         return retrofit.create(StvApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideFfzApi(@Named("ffz") retrofit: Retrofit): FfzApi {
+    fun provideFfzApi(@Named(FFZ) retrofit: Retrofit): FfzApi {
         return retrofit.create(FfzApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideChatterinoApi(@Named("chatterino") retrofit: Retrofit): ChatterinoApi {
+    fun provideChatterinoApi(@Named(CHATTERINO) retrofit: Retrofit): ChatterinoApi {
         return retrofit.create(ChatterinoApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideNopApi(@Named("nop") retrofit: Retrofit): NopApi {
+    fun provideNopApi(@Named(NOP) retrofit: Retrofit): NopApi {
         return retrofit.create(NopApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideTTSFTApi(@Named("ttsft") retrofit: Retrofit): TTSFTApi {
+    fun provideTTSFTApi(@Named(TTSFT) retrofit: Retrofit): TTSFTApi {
         return retrofit.create(TTSFTApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideTwitchingApi(@Named("twitching") retrofit: Retrofit): TwitchingApi {
+    fun provideTwitchingApi(@Named(TWITCHING) retrofit: Retrofit): TwitchingApi {
         return retrofit.create(TwitchingApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideLolApi(@Named("lol") retrofit: Retrofit): LolApi {
+    fun provideLolApi(@Named(TTVLOL) retrofit: Retrofit): LolApi {
         return retrofit.create(LolApi::class.java)
     }
 
     @ApiScope
     @Provides
     @Named("PAS1")
-    fun providePas1Api(@Named("pas1") retrofit: Retrofit): PurpleAdblockApi {
+    fun providePas1Api(@Named(PAS1) retrofit: Retrofit): PurpleAdblockApi {
         return retrofit.create(PurpleAdblockApi::class.java)
     }
 
     @ApiScope
     @Provides
     @Named("PAS2")
-    fun providePas2Api(@Named("pas2") retrofit: Retrofit): PurpleAdblockApi {
+    fun providePas2Api(@Named(PAS2) retrofit: Retrofit): PurpleAdblockApi {
         return retrofit.create(PurpleAdblockApi::class.java)
     }
 
     @ApiScope
     @Provides
-    fun provideFfzapApi(@Named("ffzap") retrofit: Retrofit): FFZAPApi {
+    fun provideFfzapApi(@Named(FFZAP) retrofit: Retrofit): FFZAPApi {
         return retrofit.create(FFZAPApi::class.java)
     }
 }

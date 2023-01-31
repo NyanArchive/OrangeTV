ca547070423d975a641e80688ff0c4267dbcfc9e
nopbreak <nopbreak@gmail.com>
2023-01-31 16:16:20 +0300

    Fix nodes (thx Z1)

--- app/core/src/main/java/tv/orange/core/models/flag/variants/Node.kt
+++ app/core/src/main/java/tv/orange/core/models/flag/variants/Node.kt
@@ -4,11 +4,26 @@ import tv.orange.core.models.flag.core.Variant
 
 enum class Node(val value: String) : Variant {
     Auto("auto"),
-    LimelightKt("limelight_kt"),
-    LimelightSk("limelight_sk"),
-    LimelightLg("limelight_lg"),
-    AkamaiKorea("akamai_korea"),
-    AkamaiNa("akamai_na");
+    L3_HLS("l3_hls"),
+    A_ASIA("akamai_asia"),
+    A_KOREA("akamai_korea"),
+    A_EU("akamai_eu"),
+    A_NA("akamai_na"),
+    A_LATAM("akamai_latam"),
+    E_ASIA("edgecast_asia"),
+    E_EU("edgecast_eu"),
+    E_NA("edgecast_na"),
+    E_LATAM("edgecast_latam"),
+    F_NA("fastly_na"),
+    F_EU("fastly_eu"),
+    F_APAC("fastly_apac"),
+    F_OCEANIA("fastly_oceania"),
+    F_LATAM("fastly_latam"),
+    C_KOREA("cloudfront_korea"),
+    C_GLOBAL("cloudfront_global"),
+    L_KT("limelight_kt"),
+    L_LG("limelight_lg"),
+    L_SK("limelight_sk");
 
     override fun getDefault(): Variant {
         return Auto
